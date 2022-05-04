b3f26525dbf520526699473e5f33b81a521ea1fc
Jidok-San <guiferreira8545@gmail.com>
2022-05-03 23:44:43 -0300

    .

--- projeto_casos.py
+++ projeto_casos.py
@@ -5,7 +5,6 @@ from fpdf import FPDF
 
 planilha = pd.read_excel("Dados-covid-19-municipios.xlsx")
 
-
 municipios = planilha['Município']
 casos = planilha['Total de casos']
 
@@ -13,14 +12,12 @@ plt.pie(casos, labels=municipios, autopct='%1.2f%%')
 
 plt.savefig("Casos de covid - 10 Municípios.png")
 
-pdf = FPDF('P', 'mm', 'A4')
+pdf = FPDF('P', 'mm', 'A3')
 pdf.add_page()
 pdf.set_font('Times', 'i', 16)
-pdf.cell(w=0, h=0, txt="Gráfico de casos de covid em 10 municípios")
-pdf.line(10, 15, 200, 15)
-
-pdf.image(name="Casos de covid - 10 Municípios.png", x=10, y=10, w=10)
+pdf.cell(w=0, h=0, txt="Casos de covid-19 - 10 Municípios", align="C")
+pdf.image(name="Casos de covid - 10 Municípios.png", x=0, y=20, w=200)
 pdf.output("Casos de covid - 10 Municípios.pdf")
 
 print("O PDF foi criado/alterado com sucesso!")
-os.system("pause")
+os.system("pause") 
---
+++ projeto_obitos.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from fpdf import FPDF
+
+planilha = pd.read_excel("Dados-covid-19-municipios.xlsx")
+
+municipios = planilha['Município']
+obitos = planilha['Total de óbitos']
+
+plt.barh(municipios, obitos , color='red', height=0.5)
+
+plt.savefig("Óbitos por covid-19 - 10 Municípios.png")
+
+pdf = FPDF('P', 'mm', 'A4')
+pdf.add_page()
+pdf.set_font('Times', 'i', 16)
+pdf.cell(w=0, h=0, txt="Óbitos por covid-19 - 10 Municípios", align='C')
+pdf.line(10, 15, 200,15)
+
+pdf.image(name="Óbitos por covid-19 - 10 Municípios.png", x=0, y=40, w=250)
+pdf.output("Óbitos por covid-19 - 10 Municípios.pdf")
+
+print("PDF criado/alterado com sucesso!")
+os.system("pause")
