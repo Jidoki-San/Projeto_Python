0ccd22f31f90dcb488fb227a29ff6293c77c1068
Jidok-San <guiferreira8545@gmail.com>
2022-05-04 01:18:10 -0300

    .

--- projeto_casos.py
+++ projeto_casos.py
@@ -8,15 +8,17 @@ planilha = pd.read_excel("Dados-covid-19-municipios.xlsx")
 municipios = planilha['Município']
 casos = planilha['Total de casos']
 
+plt.figure(figsize=(10,9))
 plt.pie(casos, labels=municipios, autopct='%1.2f%%')
-
+texto = "Casos de covid-19 \n Esse gráfico de pizza mostra as porcentagens de casos de cada município, se liga:"
 plt.savefig("Casos de covid - 10 Municípios.png")
 
-pdf = FPDF('P', 'mm', 'A3')
+pdf = FPDF('P', 'mm', 'A4')
 pdf.add_page()
-pdf.set_font('Times', 'i', 16)
-pdf.cell(w=0, h=0, txt="Casos de covid-19 - 10 Municípios", align="C")
-pdf.image(name="Casos de covid - 10 Municípios.png", x=0, y=20, w=200)
+pdf.set_font('Times', '', 14)
+pdf.multi_cell(w=0, h=7, txt=texto, align="C")
+# pdf.line(25, 30, 215, 30)
+pdf.image(name="Casos de covid - 10 Municípios.png", x=0, y=40, w=200,)
 pdf.output("Casos de covid - 10 Municípios.pdf")
 
 print("O PDF foi criado/alterado com sucesso!")
--- projeto_obitos.py
+++ projeto_obitos.py
@@ -4,12 +4,11 @@ import matplotlib.pyplot as plt
 from fpdf import FPDF
 
 planilha = pd.read_excel("Dados-covid-19-municipios.xlsx")
-
 municipios = planilha['Município']
 obitos = planilha['Total de óbitos']
 
+plt.figure(figsize=(15,14))
 plt.barh(municipios, obitos , color='red', height=0.5)
-
 plt.savefig("Óbitos por covid-19 - 10 Municípios.png")
 
 pdf = FPDF('P', 'mm', 'A4')
@@ -18,7 +17,7 @@ pdf.set_font('Times', 'i', 16)
 pdf.cell(w=0, h=0, txt="Óbitos por covid-19 - 10 Municípios", align='C')
 pdf.line(10, 15, 200,15)
 
-pdf.image(name="Óbitos por covid-19 - 10 Municípios.png", x=0, y=40, w=250)
+pdf.image(name="Óbitos por covid-19 - 10 Municípios.png", x=0, y=25, w=220)
 pdf.output("Óbitos por covid-19 - 10 Municípios.pdf")
 
 print("PDF criado/alterado com sucesso!")
